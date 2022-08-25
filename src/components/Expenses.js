69c0f80bbec4c2c6e12fb37dea77ead2aeca5831
AndroidMinsky <aleksandr.vesselko@gmail.com>
2022-08-25 15:41:48 +0100

    Card component added

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,15 @@
 import React from "react";
+import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 export default function Expenses({ data }) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItem {...data[0]} />
       <ExpenseItem {...data[1]} />
       <ExpenseItem {...data[2]} />
       <ExpenseItem {...data[3]} />
-    </div>
+    </Card>
   );
 }
