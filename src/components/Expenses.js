dff943b3a942447ecf0c366045a6adda10a2611d
AndroidMinsky <aleksandr.vesselko@gmail.com>
2022-08-27 22:36:23 +0100

    Expense Filter feature created

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expenses.css";
 
 export default function Expenses({ data }) {
+  const [year, setYear] = useState("2021");
+
+  const yearChangeHandler = function (e) {
+    setYear(e.target.value);
+  };
+
   return (
     <Card className="expenses">
+      <ExpenseFilter onYearChange={yearChangeHandler} year={year} />
       {data.map((expense) => (
         <ExpenseItem {...expense} key={expense.id} />
       ))}
